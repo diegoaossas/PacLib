a7b719cfcdc1f86150c4d4251841a2b18e1c97d1
diegoaossas <diegoaossas@gmail.com>
2015-02-06 22:35:43 -0430

    Agregada libreria para Servidor y Cliente

---
+++ src/Libreria/Actions.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Libreria;
+/**
+ *
+ * @author Diego
+ */
+
+public enum Actions
+{
+    LOGIN,
+    REGISTRO,
+    NEWLOBBY,
+    GETLOBBYS,
+    GETLOBBYSstream, GETLOBBYSstreamStop
+}
