84a5011b85c661cf7ff96fd0e2fdb0ad207349e8
diegoaossas <diegoaossas@gmail.com>
2015-02-17 17:51:29 -0430

    Limpiando

--- src/Libreria/Sala.java
+++ src/Libreria/Sala.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Libreria;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
-/**
- *
- * @author Diego
- */
-public class Sala implements Serializable{
+public class Sala implements Serializable
+{
     private static final long serialVersionUID = 1L;
     
     public long idSala;
--- src/Libreria/Usuario.java
+++ src/Libreria/Usuario.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Libreria;
 
 import java.io.Serializable;
 
-/**
- *
- * @author Diego
- */
-public class Usuario implements Serializable {
+public class Usuario implements Serializable
+{
     private static final long serialVersionUID = 1L;
     
     public int ID;
