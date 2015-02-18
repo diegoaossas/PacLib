741e198f855830144e0608c78af85da04993c1fa
diegoaossas <diegoaossas@gmail.com>
2015-02-17 21:47:32 -0430

    maximo 4 jugadores

--- src/Libreria/Sala.java
+++ src/Libreria/Sala.java
@@ -9,24 +9,22 @@ public class Sala implements Serializable
     
     public long idSala;
     public String nombreSala;
-    public int maxjugadores;
+    public int maxjugadores = 4;
     public ArrayList<Usuario> jugadores;
-    public int jugadoresEnSala;
     
     public boolean agregarJugador(Usuario usuario)
     {
-        if(jugadoresEnSala >= maxjugadores)
+        if(jugadores.size() >= maxjugadores)
             return false;
         
         jugadores.add(usuario);
-        jugadoresEnSala++;
         return true;
     }
     
     public boolean quitarJugador(Usuario usuario)
     {
-        jugadores.remove(usuario);
-        jugadoresEnSala--;
-        return true;
+        boolean eliminado = jugadores.remove(usuario);
+        System.out.println("Sala:quitarJugador() -> " + usuario.Cuenta + "("+usuario.hashCode()+") -> " + eliminado);
+        return eliminado;
     }
 }
