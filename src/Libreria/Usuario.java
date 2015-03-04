7f25e1058da963f2c47af2362896f3eaf39fd9dc
diegoaossas <diegoaossas@gmail.com>
2015-03-04 18:27:29 -0430

    Paquito revive

--- src/Libreria/Usuario.java
+++ src/Libreria/Usuario.java
@@ -34,12 +34,10 @@ public class Usuario implements Serializable
     public int hashCode()
     {
         int hash = 7;
-        
         hash = 79 * hash + this.ID;
         hash = 79 * hash + Objects.hashCode(this.Cuenta);
         hash = 79 * hash + Objects.hashCode(this.Nombre);
         hash = 79 * hash + Objects.hashCode(this.paco);
-        
         return hash;
     }
 
@@ -47,33 +45,37 @@ public class Usuario implements Serializable
     public boolean equals(Object obj)
     {
         if (obj == null)
+        {
             return false;
-        
+        }
         if (getClass() != obj.getClass())
+        {
             return false;
-        
+        }
         final Usuario other = (Usuario) obj;
-        
         if (this.ID != other.ID)
+        {
             return false;
-        
+        }
         if (!Objects.equals(this.Cuenta, other.Cuenta))
+        {
             return false;
-        
+        }
         if (!Objects.equals(this.Nombre, other.Nombre))
+        {
             return false;
-        
+        }
         if (!this.paco.equals(other.paco))
+        {
             return false;
-        
+        }
         return true;
     }
 
     @Override
     protected Usuario clone() throws CloneNotSupportedException
     {
-        Usuario usu = (Usuario) super.clone();
-        
+        Usuario usu = new Usuario();
         usu.Clave = this.Clave;
         usu.Cuenta = this.Cuenta;
         usu.ID = this.ID;
@@ -86,4 +88,6 @@ public class Usuario implements Serializable
 
         return usu;
     }
+    
+    
 }
